63290f147c868dc3f0902e2fe87056e653d52daf
justo414 <justin.lie3@hotmail.com.au>
2022-11-21 09:55:56 +1100

    seedDatabase

--- StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Address.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Address.cs
@@ -5,8 +5,8 @@ namespace StudentAdminPortal.API.DataModels
     public class Address
     {
         public Guid Id { get; set; }
-        public string PhysicalAddress { get; set; }
-        public string PostalAddress { get; set; }
+        public string? PhysicalAddress { get; set; }
+        public string? PostalAddress { get; set; }
 
         // Navigation Property
         public Guid StudentId { get; set; }
--- StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Student.cs
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Student.cs
@@ -12,7 +12,7 @@ namespace StudentAdminPortal.API.DataModels
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public long Mobile { get; set; }
-        public string ProfileImageUrl { get; set; }
+        public string? ProfileImageUrl { get; set; }
         public Guid GenderId { get; set; }
 
         // Navigation Properties
